333ab071d1b0864046ebb229339f562c3fb3efca
MainDavis <44068483+MainDavis@users.noreply.github.com>
2021-10-07 14:16:23 +0200

    CMake mas o menos

---
+++ build/CMakeFiles/multMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multMatrix.dir/multMatrix/main_multMatrix.cpp.o"
+  "CMakeFiles/multMatrix.dir/multMatrix/multmatrix.cpp.o"
+  "multMatrix"
+  "multMatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
